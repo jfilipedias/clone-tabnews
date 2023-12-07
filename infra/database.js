ffd293f8d9db4c52fe8d621e1b0a2149bf20b298
Filipe Dias <filipediascontato@gmail.com>
2023-12-07 22:12:46 +0000

    feat: Add a connection pool to database module

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,23 @@
-import { Client } from "pg";
+import { Pool } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  const pool = new Pool({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  const client = await pool.connect();
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (err) {
+    throw err;
+  } finally {
+    client.release();
+  }
 }
 
 export default {
