7817ab5c514c4b47fffe57640d033fabc8ac4060
Filipe Dias <filipediascontato@gmail.com>
2023-12-07 22:14:34 +0000

    feat: Return the first results from services status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,30 @@
 import database from "infra/database.js";
 
 export default async function handler(req, res) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
+  const updatedAt = new Date().toISOString();
 
-  res.status(200).json({ message: "Alunos do curso.dev são acima da média" });
+  const serverVersionResult = await database.query("SHOW server_version;");
+  const serverVersionValue = serverVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnectionsResult.rows[0].max_connections;
+
+  const openedConnectionsResult = await database.query(
+    "SELECT COUNT (*) AS opened_connections from pg_stat_activity WHERE state = 'active';",
+  );
+  const openedConnectionsValue =
+    openedConnectionsResult.rows[0].opened_connections;
+
+  const apiStatus = {
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: serverVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: parseInt(openedConnectionsValue),
+      },
+    },
+  };
+
+  res.status(200).json(apiStatus);
 }
